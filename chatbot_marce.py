936841e325e08fd7c74dd9348227dd300cb904c1
Mar00z1 <marotrexsl165@gmail.com>
2025-08-06 11:03:53 -0300

    Update chatbot_marce.py

--- chatbot_marce.py
+++ chatbot_marce.py
@@ -107,9 +107,6 @@ def webhook():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     logging.info(f"Arrancando servidor en 0.0.0.0:{port}")
-    app.run(host='0.0.0.0', port=port) == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    logging.info(f"Arrancando servidor en 0.0.0.0:{port}")
     app.run(host='0.0.0.0', port=port)
 
 
